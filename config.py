2e1f365a77231f57b27190606d6a38b8abdeb337
Joe <jhemmerle06@gmail.com>
2026-07-30 11:03:18 -0500

    re-integrate dotenv structure

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+import os
 
-AI_ENDPOINT = "http://10.0.0.185:5050/v1"
-AI_API_KEY = "x"
-AI_MODEL = "current"
+load_dotenv()
+
+AI_ENDPOINT = os.getenv("AI_ENDPOINT", "http://aimodel.lan/v1")
+AI_API_KEY = os.getenv("AI_API_KEY", "x")
+AI_MODEL = os.getenv("AI_MODEL", "current")
 DIFFICULTIES = ["Easy", "Medium", "Hard"]
 MAX_TOKENS = 4096
